09d48d451cffe6ae4b77a08ec91eea0935bbad37
fungame2270 <tomasvictal@gmail.com>
2025-12-19 17:00:22 +0000

    added flast asked endpoint

--- browser_extension/background.js
+++ browser_extension/background.js
@@ -1,6 +1,7 @@
 // background.js
 let isEnabled = true;
 let allowedDomains = [];
+const apiEndpoint = "http://127.0.0.1:3000";
 
 browser.storage.local.get({ extensionEnabled: true, allowedDomains: [] }).then((data) => {
   isEnabled = data.extensionEnabled;
@@ -39,23 +40,44 @@ async function handleRequest(details) {
   if (url.startsWith("moz-extension://")) {
     return { cancel: false };
   }
-  //  TODO: Change to api Call
-  const isBadUrl = url.includes("print"); 
 
-  if (isBadUrl) {
-    console.log("Bad URL detected.");
-    browser.tabs.update(details.tabId, {
-      url: browser.runtime.getURL("blocked.html?target="+ url)
+  try{
+    //  TODO: Change to api Call
+    const isBadUrl = url.includes("print");
+    if (isBadUrl) {
+      console.log("Bad URL detected.");
+      browser.tabs.update(details.tabId, {
+        url: browser.runtime.getURL("blocked.html?target="+ url)
+      });
+      
+      return { cancel: true };
+    }
+    
+    const data = await fetch(apiEndpoint, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) =>{
+      if (!response.ok){
+        throw new Error("API Server Error: " + response.status);
+      }
+      return response.json()
     });
     
-    return { cancel: true };
+    console.log(data)
   }
+  catch (error) {
+    console.error("Network error checking URL:", error);
+    return { cancel: true }; 
+  }
+
   console.log("All Good!!!")
   return { cancel: false };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   handleRequest,
-  { urls: ["<all_urls>"], types: ["main_frame"] }, // Only check main page loads
+  { urls: ["<all_urls>"], types: ["main_frame"] },
   ["blocking"]
 );
